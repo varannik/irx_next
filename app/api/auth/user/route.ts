f87bde4dd7f9f537e9d52b8eca1853335bb247bd
varannik <varanik@live.com>
2024-11-07 22:38:12 +0330

    Update user api route to get getServerSession

--- app/api/auth/user/route.ts
+++ app/api/auth/user/route.ts
@@ -1,36 +1,37 @@
 
-import { getSession } from 'next-auth/react';
-import { NextApiRequest, NextApiResponse } from 'next';
+// import { getSession } from 'next-auth/react';
+// import { NextApiRequest, NextApiResponse } from 'next';
 
-export async function GET(req:NextApiRequest,  res:NextApiResponse ) {
+// export async function GET(req:NextApiRequest,  res:NextApiResponse ) {
 
-  // Get the user's session
-  const session = await getSession({ req });
-
-  if (!session) {
-
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
-
-  // Authorized: Proceed with the API logic
-  return res.status(200).json({ message: 'Protected data', user: session.user });
-
-}
-
-
-
-
-// export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-//   // Retrieve the session
+//   // Get the user's session
 //   const session = await getSession({ req });
 
 //   if (!session) {
+
 //     return res.status(401).json({ message: 'Unauthorized' });
 //   }
 
 //   // Authorized: Proceed with the API logic
-//   res.status(200).json({ message: 'Protected data', user: session.user });
+//   return res.status(200).json({ message: 'Protected data', user: session.user });
+
 // }
 
 
+
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req:NextRequest) {
+  const session = await getServerSession();
+
+  if (!session) {
+    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+  }
+
+  return NextResponse.json(
+    { message: 'Protected data', user: session.user },
+    { status: 200 }
+  );
+}
 
