80975f73e044cfae362a02ecf068a2394921f023
varannik <varanik@live.com>
2024-11-07 22:31:32 +0330

    Add domain images to nextjs config

--- app/api/auth/user/route.ts
+++ app/api/auth/user/route.ts
@@ -1,26 +1,19 @@
-// app/api/maxmin/route.ts
+
 import { getSession } from 'next-auth/react';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { NextRequest, NextResponse } from 'next/server';
-// import { connectDB } from '@/utils/db'
-// import User from '@/models/User';
-// import { getSession } from 'next-auth/react';
-
 
 export async function GET(req:NextApiRequest,  res:NextApiResponse ) {
 
   // Get the user's session
   const session = await getSession({ req });
-  console.log(session)
+
   if (!session) {
-    console.log('Bega')
-    return NextResponse.json({ message: 'User dosent exist'}, { status: 404 });
+
+    return res.status(401).json({ message: 'Unauthorized' });
   }
-  console.log(session)
+
   // Authorized: Proceed with the API logic
-  return NextResponse.json({ message: 'Protected data', user: session.user }, { status: 200 });
-
-
+  return res.status(200).json({ message: 'Protected data', user: session.user });
 
 }
 
