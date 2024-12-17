ac23533617a7a868c95a8c0663738b6b84aa3e0a
varannik <varanik@live.com>
2024-12-17 17:53:55 +0330

    Remove SideBarMenu

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,21 +1,16 @@
 import LoginPage from './SigninProfile'
 import { Footer } from './Footer'
 import SelectAsset from './SelectAsset'
-import SidebarMenu from './SidebarMenu'
 import Header from './Header'
 
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-
-
         <Header />
-        <SidebarMenu />
         <LoginPage />
         <SelectAsset />
         <Footer />
-
         <main >
           <div >{children}</div>
         </main>
