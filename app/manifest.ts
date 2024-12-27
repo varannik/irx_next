51b77fe645a61e41e83bcb08dfafae50ed445352
varannik <varanik@live.com>
2024-12-27 13:43:19 +0330

    Change favicon and manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -3,26 +3,26 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Quantical',
-    short_name: 'QT',
+    name: "IRX by Quantical",
+    short_name: "IRX",
     description: 'Real-Time Exchange Tracker, Live Iranian Free Market Rates',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
+    start_url: '/analytics',
     icons: [
-        {
-            "src": "/web-app-manifest-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
-          },
-          {
-            "src": "/web-app-manifest-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
-          }
+      {
+        "src": "/web-app-manifest-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png",
+        "purpose": "maskable"
+      },
+      {
+        "src": "/web-app-manifest-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png",
+        "purpose": "maskable"
+      }
     ],
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
   }
 }
