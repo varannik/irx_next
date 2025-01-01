de883ca72886953760de875ed661dd6f38cfe8e7
varannik <varanik@live.com>
2025-01-01 14:39:30 +0330

    Refine route get prediction daily

--- app/api/predictions/[submitDate]/route.ts
+++ app/api/predictions/[submitDate]/route.ts
@@ -14,7 +14,11 @@ export async function GET(req: NextRequest, { params }: { params: { submitDate: 
         const {submitDate} = params;
       try {
         const c = await getCollection('UserDailyPredicts');
-        const query = {submitDate: parseDate(submitDate)}
+
+        const startDate = new Date(`${parseDate(submitDate)}T00:00:00Z`)
+        const endDate = new Date(`${parseDate(submitDate)}T23:59:59Z`)
+
+        const query = {submitDate:{ $gte: startDate, $lt: endDate }}
         const prediction = await c.find(query).toArray();
         return NextResponse.json({ success: true, data: prediction }, { status: 200 });
       } catch (error) {
